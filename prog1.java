28833773cfe43f79ce74f319d6dcf1a2146e2dd5
pavan1832 <141185596+pavan1832@users.noreply.github.com>
2024-06-01 09:04:16 +0530

    Update prog1.java

--- prog1.java
+++ prog1.java
@@ -1,80 +1,77 @@
-//W.A.P to implement frames using appropriate sorting techniques
-
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.*;
-
-class Frame {
-
-    int seqNo;
-    String data;
-
-    public Frame(int seqNo) {
-        this.seqNo = seqNo;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration Page</title>
+  <style>
+    body{
+        background-color: purple;
+        color: white;
     }
-
-}
-
-public class Framesort {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ENTER THE TOTAL SIZE OF THE MESSAGE");
-        int total = sc.nextInt();
-
-        System.out.println("ENTER THE SIZE OF EACH DATA PACKET");
-        int size = sc.nextInt();
-
-        int n = total / size;
-        List<Frame> frames = new ArrayList<Frame>();
-        for (int i = 0; i < n; i++) {
-            frames.add(new Frame(i));
-        }
-
-        for (Frame f : frames) {
-            System.out.println("ENTER THE DATA FOR EACH FRAME " + f.seqNo);
-            f.data = sc.next();
-        }
-        Collections.shuffle(frames);
-        System.out.println("Before sorting the frames");
-        for (Frame f : frames) {
-            System.out.println("THE SEQNO " + f.seqNo + " MESSAGE " + f.data);
-        }
-        Collections.sort(frames, (f1, f2) -> Integer.compare(f1.seqNo, f2.seqNo));
-
-        System.out.println("AFTER sorting the frames");
-        for (Frame f : frames) {
-            System.out.println("THE SEQNO " + f.seqNo + " MESSAGE " + f.data);
-        }
-
-        sc.close();
-
+    h1{
+        text-align: center;
     }
-}
-//OUTPUT
-ENTER THE TOTAL SIZE OF THE MESSAGE
-50
-ENTER THE SIZE OF EACH DATA PACKET
-10
-ENTER THE DATA FOR EACH FRAME 0
-a
-ENTER THE DATA FOR EACH FRAME 1
-b
-ENTER THE DATA FOR EACH FRAME 2
-c
-ENTER THE DATA FOR EACH FRAME 3
-d
-ENTER THE DATA FOR EACH FRAME 4
-e
-Before sorting the frames
-THE SEQNO 1 MESSAGE b
-THE SEQNO 0 MESSAGE a
-THE SEQNO 3 MESSAGE d
-THE SEQNO 2 MESSAGE c
-THE SEQNO 4 MESSAGE e
-AFTER sorting the frames
-THE SEQNO 0 MESSAGE a
-THE SEQNO 1 MESSAGE b
-THE SEQNO 2 MESSAGE c
-THE SEQNO 3 MESSAGE d
-THE SEQNO 4 MESSAGE e
+    form{
+        display: block;
+    }
+  </style>
+  <script>
+    function validateForm() {
+      // Get form values
+      var userId = document.getElementById("userId").value;
+      var name = document.getElementById("name").value;
+      var password = document.getElementById("password").value;
+      var email = document.getElementById("email").value;
+      
+      // User ID validation
+      if (userId.length < 5 || userId.length > 12) {
+        alert("User ID must be between 5 and 12 characters long");
+        return;
+      }
+      
+      // Name validation
+      if (name.length < 10 || !/^[a-zA-Z ]+$/.test(name)) {
+        alert("Name must be at least 10 characters long and contain only alphabets");
+       return;
+      }
+      
+      // Password validation
+      var passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/;
+      if (!passwordRegex.test(password)) {
+        alert("Password must be eight characters including one uppercase letter, one special character, and alphanumeric characters");
+        return;
+      }
+      
+      // Email validation
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        alert("Invalid email address");
+        return;
+      }
+
+      let details= "User ID: " + userId + "\nName: " + name + "\nPassword: " + password +"\nEmail: " + email;
+      alert(`${details}`)
+    }
+  </script>
+</head>
+<body>
+  <h1>Registration Page</h1>
+  <form name="registrationForm" align="center">
+    <label for="userId">User ID:</label>
+    <input type="text" id="userId" name="userId"><br><br>
+    
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" ><br><br>
+    
+    <label for="password">Password:</label>
+    <input type="text" id="password" name="password"><br><br>
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email"><br><br>
+    
+   <button onclick="validateForm()">Submit</button>
+  </form>
+
+  <br>
+
+</body>
+</html>
